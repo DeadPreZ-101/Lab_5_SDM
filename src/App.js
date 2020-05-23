01b549e0b9c0a7b70c8aa7377fa2cee24839546d
Taj Sivers <tajsivers@live.com>
2020-05-23 15:17:27 -0400

    Taj Basic Form

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+                <div className="App"/>
+          <h1>Contact Form</h1>
+
+<div class="container" />
+  <form action="?" id="border"/>
+    <label for="fname">First Name</label><br/>
+    <input type="text" id="fname" name="firstname"  placeholder="Your name.."/><br/><br/>
+
+    <label for="lname">Last Name</label><br/>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name.."/><br/><br/>
+
+    <label for="country">Country</label><br/>
+    <select id="country" name="country"><br/>
+        <option value="canada">Canada</option>
+        <option value="australia">Australia</option>
+        <option value="usa">USA</option>
+    </select><br/><br/>
+
+    <label for="FAQ">FAQ</label><br/>
+    <textarea id="FAQ" name="FAQ" placeholder="Type your question here.." style={{width: 1000, height: 200}}></textarea><br/><br/>
+
+    <input type="submit" value="Submit" id="ID"/>
+  <form/>
+<div/>
     </div>
   );
 }

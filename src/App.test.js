b9fa3f29afc8e5772bd4330c49c22205128f8d40
DeadPrez-101 <al.chemist.jasat@gmail.com>
2020-05-23 19:49:22 -0400

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,11 @@ import { render } from "@testing-library/react";
 import Feature from "./Feature";
 import Joke from "./test1";
 
-test("Feature component receives props and then render the text", () => {
-  const { getByTestId } = render(<Feature text="Contact Form" />);
+// test("Feature component receives props and then render the text", () => {
+//   const { getByTestId } = render(<Feature text="Contact Form" />);
 
-  expect(getByTestId("feature-text")).toHaveTextContent("Contact Form");
-});
+//   expect(getByTestId("feature-text")).toHaveTextContent("Contact Form");
+// });
 
 test("Joke component receives props and then render the text", () => {
   const { getByTestId } = render(

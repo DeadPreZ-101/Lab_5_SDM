1df162a258fdf256ef225c3297dd598c02ed7cf0
DeadPrez-101 <al.chemist.jasat@gmail.com>
2020-05-23 15:33:20 -0400

    updated feature

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>TEAM 9 MEMBERS:
-      <br/>
-      <br/>
-        Zakariya J: 
-                <br/>
-                Zakariya has traveled through numerous nations across Africa,
-                Asia, Europe and North America; with work experience in Canada,
-                England and Rwanda. Living in multiple environments and working
-                with industry professionals from several cultures has empowered
-                Zakariya in being a talented business manager with demonstrated
-                experience in adapting to change, creating strategies for
-                organizational success, creative problem solving and viewing
-                life through numerous lenses. He enjoys new challenges and was
-                interested in learning more about Blockchain Development, and so
-                he has enrolled in the BCDV program at GBC!
-                <br/>
-                <br/>
-                
-        Taj S: 
-                <br/>
-                Taj is Barbadian. He studied Information Technology at 
-                the University Of the West Indies in Jamaica, and has work
-                experience with the government of Barbados. His interest in Blockchain
-                stems from a vigorous entrepreneurial spirit and a hunger to improve
-                his country technoogically. Many describe him as an approachable 
-                people person who enjoys new challenges and learning new skills.
-                <br/>
-                <br/>
-                Shafin R: 
-                <br/>
-                Shafin has studied numerous fields ranging from civil engineering to human kinetics,
-                and even art school. His exposure to different academic pursuits has allowed him an
-                understanding of the world inspiring him to make an impact by participating in a global
-                revolution. He was first intrigued by blockchain in 2019 and ever since then he has
-                been sure that this is the revolution that is most relevant to the world he would like to
-                see in the future. As a result he has enrolled in the blockchain development program
-                at George Brown College in Toronto, Ontario.
-                <br/>
-                <br/>
+      
+      <p>
+        TEAM 9 MEMBERS:
+        <br />
+        <br />
+        Zakariya J:
+        <br />
+        Zakariya has traveled through numerous nations across Africa, Asia,
+        Europe and North America; with work experience in Canada, England and
+        Rwanda. Living in multiple environments and working with industry
+        professionals from several cultures has empowered Zakariya in being a
+        talented business manager with demonstrated experience in adapting to
+        change, creating strategies for organizational success, creative problem
+        solving and viewing life through numerous lenses. He enjoys new
+        challenges and was interested in learning more about Blockchain
+        Development, and so he has enrolled in the BCDV program at GBC!
+        <br />
+        <br />
+        Taj S:
+        <br />
+        Taj is Barbadian. He studied Information Technology at the University Of
+        the West Indies in Jamaica, and has work experience with the government
+        of Barbados. His interest in Blockchain stems from a vigorous
+        entrepreneurial spirit and a hunger to improve his country
+        technoogically. Many describe him as an approachable people person who
+        enjoys new challenges and learning new skills.
+        <br />
+        <br />
+        Shafin R:
+        <br />
+        Shafin has studied numerous fields ranging from civil engineering to
+        human kinetics, and even art school. His exposure to different academic
+        pursuits has allowed him an understanding of the world inspiring him to
+        make an impact by participating in a global revolution. He was first
+        intrigued by blockchain in 2019 and ever since then he has been sure
+        that this is the revolution that is most relevant to the world he would
+        like to see in the future. As a result he has enrolled in the blockchain
+        development program at George Brown College in Toronto, Ontario.
+        <br />
+        <br />
       </p>
     </div>
   );

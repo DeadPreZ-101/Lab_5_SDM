416ca0fcf1af314b8427211ec440037285561f57
DeadPrez-101 <al.chemist.jasat@gmail.com>
2020-05-23 19:38:48 -0400

    feature fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Feature from './Feature';
-import Joke from './test1';
+import React from "react";
+import { render } from "@testing-library/react";
+import Feature from "./Feature";
+import Joke from "./test1";
 
 test("Feature component receives props and then render the text", () => {
-  const { getByTestId } = render(
-        <Feature text="Contact Form" />  );
+  const { getByTestId } = render(<Feature text="Contact Form" />);
 
-  expect(getByTestId("feature-text")).toHaveTextContent("Contact Form");});
+  expect(getByTestId("feature-text")).toHaveTextContent("Contact Form");
+});
 
 test("Joke component receives props and then render the text", () => {
   const { getByTestId } = render(

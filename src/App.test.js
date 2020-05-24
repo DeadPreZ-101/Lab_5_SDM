14e42d74674753815294217dacd8cb9a2e354f93
connova <shafdude@hotmail.com>
2020-05-23 21:09:29 -0400

    added third test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Feature from "./Feature";
 import Joke from "./test1";
+import Test from "./test3";
 
 test("Feature component receives props and then render the text", () => {
   const { getByTestId } = render(<Feature text="Contact Form" />);
@@ -16,4 +17,10 @@ test("Joke component receives props and then render the text", () => {
   expect(getByTestId("joke-text")).toHaveTextContent(
     "The funniest joke of this year."
   );
+});
+
+test("Checkin Homepage to see if it has correct text", () => {
+  const { getByTestId } = render(<Test text="Team 9" />);
+
+  expect(getByTestId("test-3")).toHaveTextContent("Team 9");
 });
---
+++ src/test3.js
@@ -0,0 +1,2 @@
+import React from "react";
+export default ({ text }) => <div data-testid="test-3">{text}</div>;
